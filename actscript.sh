aa0b6ec9db5e1dc9f6b7386433c1b8e3ac7fcade
Kraker <kraker@example.com>
2026-03-28 18:19:52 -0500

    Liberación de panel VPS-MX (Versión Personal)

---
+++ actscript.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+cd $HOME
+SCPdir="/etc/newadm"
+SCPinstal="$HOME/install"
+SCPidioma="${SCPdir}/idioma"
+SCPusr="${SCPdir}/ger-user"
+SCPfrm="/etc/ger-frm"
+SCPinst="/etc/ger-inst"
+[[ $(dpkg --get-selections|grep -w "gawk"|head -1) ]] || apt-get install gawk -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "mlocate"|head -1) ]] || apt-get install mlocate -y &>/dev/null
+rm $(pwd)/$0 &> /dev/null
+msg () {
+BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
+AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCOR='\e[0m'
+ case $1 in
+  -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
+  -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -verm)cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -bra)cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}";;
+  "-bar2"|"-bar")cor="${VERMELHO}======================================================" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
+ esac
+}
+fun_ip () {
+MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MIP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+}
+
+function_verify () {
+  # Bypass de verificación para uso personal
+  echo "OK-OFI-@kalix1" > /usr/bin/lickal &>/dev/null
+  echo "Sistem" > /usr/share/locale/sistem &>/dev/null
+  v1="Personal-Edition"
+  echo "$v1" > /etc/versin_script
+}
+
+funcao_idioma () {
+msg -bar2
+declare -A idioma=( [1]="en English" [2]="fr Franch" [3]="de German" [4]="it Italian" [5]="pl Polish" [6]="pt Portuguese" [7]="es Spanish" [8]="tr Turkish" )
+for ((i=1; i<=12; i++)); do
+valor1="$(echo ${idioma[$i]}|cut -d' ' -f2)"
+[[ -z $valor1 ]] && break
+valor1="\033[1;32m[$i] > \033[1;33m$valor1"
+    while [[ ${#valor1} -lt 37 ]]; do
+       valor1=$valor1" "
+    done
+echo -ne "$valor1"
+let i++
+valor2="$(echo ${idioma[$i]}|cut -d' ' -f2)"
+[[ -z $valor2 ]] && {
+   echo -e " "
+   break
+   }
+valor2="\033[1;32m[$i] > \033[1;33m$valor2"
+     while [[ ${#valor2} -lt 37 ]]; do
+        valor2=$valor2" "
+     done
+echo -ne "$valor2"
+let i++
+valor3="$(echo ${idioma[$i]}|cut -d' ' -f2)"
+[[ -z $valor3 ]] && {
+   echo -e " "
+   break
+   }
+valor3="\033[1;32m[$i] > \033[1;33m$valor3"
+     while [[ ${#valor3} -lt 37 ]]; do
+        valor3=$valor3" "
+     done
+echo -e "$valor3"
+done
+msg -bar2
+unset selection
+while [[ ${selection} != @([1-8]) ]]; do
+echo -ne "\033[1;37mSELECT: " && read selection
+tput cuu1 && tput dl1
+done
+pv="$(echo ${idioma[$selection]}|cut -d' ' -f1)"
+[[ ${#id} -gt 2 ]] && id="es" || id="$pv"
+byinst="true"
+}
+install_fim () {
+msg -ama "$(source trans -b es:${id} "Instalacion Completa, Utilize los Comandos"|sed -e 's/[^a-z -]//ig')" && msg bar2
+
+msg -bar2
+}
+ofus () {
+unset server
+server=$(echo ${txt_ofuscatw}|cut -d':' -f1)
+unset txtofus
+number=$(expr length $1)
+for((i=1; i<$number+1; i++)); do
+txt[$i]=$(echo "$1" | cut -b $i)
+case ${txt[$i]} in
+".")txt[$i]="+";;
+"+")txt[$i]=".";;
+"1")txt[$i]="@";;
+"@")txt[$i]="1";;
+"2")txt[$i]="?";;
+"?")txt[$i]="2";;
+"4")txt[$i]="%";;
+"%")txt[$i]="4";;
+"-")txt[$i]="K";;
+"K")txt[$i]="-";;
+esac
+txtofus+="${txt[$i]}"
+done
+echo "$txtofus" | rev
+}
+verificar_arq () {
+[[ ! -d ${SCPdir} ]] && mkdir ${SCPdir}
+[[ ! -d ${SCPusr} ]] && mkdir ${SCPusr}
+[[ ! -d ${SCPfrm} ]] && mkdir ${SCPfrm}
+[[ ! -d ${SCPinst} ]] && mkdir ${SCPinst}
+case $1 in
+"menu"|"message.txt")ARQ="${SCPdir}/";; #Menu
+"usercodes")ARQ="${SCPusr}/";; #Panel SSRR
+"C-SSR.sh")ARQ="${SCPinst}/";; #Instalacao
+"openssh.sh")ARQ="${SCPinst}/";; #Instalacao
+"squid.sh")ARQ="${SCPinst}/";; #Instalacao
+"dropbear.sh")ARQ="${SCPinst}/";; #Instalacao
+"openvpn.sh")ARQ="${SCPinst}/";; #Instalacao
+"ssl.sh")ARQ="${SCPinst}/";; #Instalacao
+"shadowsocks.sh")ARQ="${SCPinst}/";; #Instalacao
+"Shadowsocks-libev.sh")ARQ="${SCPinst}/";; #Instalacao
+"Shadowsocks-R.sh")ARQ="${SCPinst}/";; #Instalacao 
+"v2ray.sh")ARQ="${SCPinst}/";; #Instalacao
+"budp.sh")ARQ="${SCPinst}/";; #Instalacao
+"sockspy.sh"|"PDirect.py"|"PPub.py"|"PPriv.py"|"POpen.py"|"PGet.py")ARQ="${SCPinst}/";; #Instalacao
+*)ARQ="${SCPfrm}/";; #Ferramentas
+esac
+mv -f ${SCPinstal}/$1 ${ARQ}/$1
+chmod +x ${ARQ}/$1
+}
+fun_ip
+wget -O /usr/bin/trans https://www.dropbox.com/s/wfrbkbzhqnyoehc/trans &> /dev/null
+wget -O /bin/Desbloqueo.sh https://www.dropbox.com/s/b5uwfw5i38set36/Desbloqueo.sh &> /dev/null
+chmod +x /bin/Desbloqueo.sh
+wget -O /bin/monitor.sh https://www.dropbox.com/s/pp6kqc4t08nxlhc/monitor.sh &> /dev/null
+chmod +x /bin/monitor.sh
+wget -O /var/www/html/estilos.css https://www.dropbox.com/s/vzx4rfxqds1btt4/estilos.css &> /dev/null
+clear
+msg -bar2
+echo -e "\e[97m\033[1;41m       =====>>►► 🐲 PANEL VPS•MX 🐲 ◄◄<<=====       \033[1;37m"
+msg -bar2
+[[ $1 = "" ]] && funcao_idioma || {
+[[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
+ }
+error_fun () {
+msg -bar2 && msg -verm "$(source trans -b es:${id} "Esta Key Es de Otro Servidor Por Lo Tanto Fue Excluida"|sed -e 's/[^a-z -]//ig') " && msg -bar2
+[[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}
+exit 1
+}
+invalid_key () {
+msg -bar2 && msg -verm "#¡Key Invalida#! " && msg -bar2
+[[ -e $HOME/lista-arq ]] && rm $HOME/lista-arq
+exit 1
+}
+# Comentando la petición de Key para uso personal
+# while [[ ! $Key ]]; do
+# msg -ne "##Digite  Key##: " && read Key
+# tput cuu1 && tput dl1
+# done
+msg -ne "# Modo Personal Activo # : "
+echo -e "\033[1;32m Verificado"
+
+# Configura aquí la IP de tu servidor Master y el ID de requerimiento
+# Por defecto buscamos en el mismo servidor si estás replicando localmente
+IP="127.0.0.1" # Cambia esto por la IP de tu Master
+REQUEST="Oficial" # Cambia esto por tu ID de carpeta en el Master
+echo "$IP" > /usr/bin/vendor_code
+sleep 1s
+function_verify
+updatedb
+if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq|grep "KEY INVALIDA!") ]]; then
+   msg -bar2
+   msg -verd "$(source trans -b es:${id} " INSTALANDO"|sed -e 's/[^a-z -]//ig'): \033[1;31m[VPS-MX #MOD by @Kalix1]"
+   REQUEST=$(ofus "$Key"|cut -d'/' -f2)
+   [[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
+   pontos="."
+   stopping="$(source trans -b es:${id} "Verificando Actualizaciones"|sed -e 's/[^a-z -]//ig')"
+   for arqx in $(cat $HOME/lista-arq); do
+   msg -verm "${stopping}${pontos}"
+   wget -O ${SCPinstal}/${arqx} ${IP}:8888/${REQUEST}/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}" || error_fun
+   tput cuu1 && tput dl1
+   pontos+="."
+   done
+   sleep 1s
+   msg -bar2
+   listaarqs="$(locate "lista-arq"|head -1)" && [[ -e ${listaarqs} ]] && rm $listaarqs   
+   cat /etc/bash.bashrc|grep -v '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' > /etc/bash.bashrc.2
+   echo -e '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' >> /etc/bash.bashrc.2
+   mv -f /etc/bash.bashrc.2 /etc/bash.bashrc
+   echo "${SCPdir}/menu" > /usr/bin/menu && chmod +x /usr/bin/menu
+   echo "${SCPdir}/menu" > /usr/bin/adm && chmod +x /usr/bin/adm
+   echo "$Key" > ${SCPdir}/key.txt
+   [[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}   
+   [[ ${#id} -gt 2 ]] && echo "es" > ${SCPidioma} || echo "${id}" > ${SCPidioma}
+   [[ ${byinst} = "true" ]] && install_fim
+else
+invalid_key
+
+fi
